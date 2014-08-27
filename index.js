61139f952aca30e996c7dd2fe5352a33aed2fee6
Michael Lawrence <michael.lawrence@rhythmagency.com>
2014-08-27 15:04:23 -0700

    Fixing test path.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var Mocha = require('mocha'),
+	path = require('path'),
 	mocha = new Mocha();
 
 if (process.argv && process.argv.length > 2) {
 	process.env.RHYTHM_DEPLOYMENT_TEST_DOMAIN = process.argv[2];
 
-	mocha.addFile('test.js');
+	mocha.addFile(path.join(__dirname, 'test.js'));
 
 	mocha.run(function (failures) {
 		process.on('exit', function () {
